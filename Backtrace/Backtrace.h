7d26ff02a339a3507d1b1433fab4c56cb18162b6
Kirizaki <krajekg@gmail.com>
2016-12-29 23:40:44 +0100

    add exception and exception handling

--- Backtrace/Backtrace.h
+++ Backtrace/Backtrace.h
@@ -10,27 +10,24 @@
 class Backtrace
 {
 public:
-   Backtrace(int maxDepth = 32);
+   Backtrace(const int& maxDepth = 32);
    ~Backtrace();
    std::string GetBacktrace();
 
 private:
    void     Callstack();
-   void     LoadModule();
-   void     LoadDBGHELP();
-   void     LoadModuleInformation(HANDLE hProcess);
    int      maxDepth;
-   DWORD    LoadModule(HANDLE hProcess, const char modulePath[], DWORD64 baseAddr, DWORD size);
 
    HANDLE   hProcess;
    DWORD    dwProcessId;
-   HMODULE  hDbhHelp;
+   HMODULE  hDbgHelp;
 
    typedef struct Entry
    {
       std::string function;
       DWORD       line;
       std::string file;
+      int         count;
    };
    std::vector<Entry> callStack;
 };
